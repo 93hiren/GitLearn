809d0cc3f121e70129e5ed4fab4a5d9d81bbf751
Hiren <galahiren3@gmail.com>
2024-10-26 13:48:57 +0530

    After lunch saturday

--- GitBasics/src/Main.java
+++ GitBasics/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        String input = "mphasis";
+        StringBuilder sb = new StringBuilder();
+        for(Character ch:input.toCharArray()){
+
+            sb.append(ch);
+            if(ch == 'a'){
+                sb.append("abc");
+
+            }
+
+        }
+        System.out.println(sb.toString());
+
+
     }
+
+
 }
--- GitBasics/src/test.java
+++ GitBasics/src/test.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class test {
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("I am in test class changed this morning");
+        System.out.println("After lunch saturday");
+    }
 }
