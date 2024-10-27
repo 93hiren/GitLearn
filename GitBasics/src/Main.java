eabf49e2cd15cb62c04b8f98e9d82a3c0cb58a66
Hiren <galahiren3@gmail.com>
2024-10-27 16:58:59 +0530

    second main

--- GitBasics/src/Main.java
+++ GitBasics/src/Main.java
@@ -20,20 +20,7 @@ public class Main {
         }
         System.out.println(sb.toString());
 
-        System.out.println("Today I gave 2 interviews for the product company and they offered me 70LPA" +
-                " on 10/26/2026");
-        System.out.println("I married the beautifull  Charming young girl who I am truly,deeply " +
-                "and madly in love with her");
-
-        System.out.println("Working Sunday and I love it after 1 month or so I will enjoy football" +
-                "and swiming ");
-
-        System.out.println("Gratefull for evrything that happend thank you so much");
-
-        System.out.println("Sunday : Favorite City Mancity");
-        System.out.println("sunday with football ");
-
-        System.out.println("Main Class El-Class-ico");
+        System.out.println("I am in Main Class");
 
 
     }
