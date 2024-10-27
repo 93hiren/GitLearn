d43e027ed300e397b4ef5cf6604384c01d2e856e
Hiren <galahiren3@gmail.com>
2024-10-27 15:20:49 +0530

    Sunday afternoon

--- GitBasics/src/Main.java
+++ GitBasics/src/Main.java
@@ -28,6 +28,8 @@ public class Main {
         System.out.println("Working Sunday and I love it after 1 month or so I will enjoy football" +
                 "and swiming ");
 
+        System.out.println("Gratefull for evrything that happend thank you so much");
+
 
     }
 
