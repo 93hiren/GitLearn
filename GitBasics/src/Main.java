d3f338acb023d54155758e4e8b84a4218ece55e6
Hiren <galahiren3@gmail.com>
2024-10-28 06:34:09 +0530

    second main

--- GitBasics/src/Main.java
+++ GitBasics/src/Main.java
@@ -26,7 +26,7 @@ public class Main {
         System.out.println("Working Sunday and I love it after 1 month or so I will enjoy football" +
                 "and swiming ");
 
-        System.out.println("Merging from third main");
+        System.out.println("Merging from third main on Monday");
         System.out.println("Gratefull for evrything that happend thank you so much for such divine blessing");
 
         System.out.println("Sunday : Favorite City Mancity Football Club and one day I will watch love match in ethiad stadium");
