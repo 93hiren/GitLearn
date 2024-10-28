8809c9359052618f15645a5651bf41f12ffb69c1
Hiren <galahiren3@gmail.com>
2024-10-28 13:41:59 +0530

    today morning

--- GitBasics/src/Main.java
+++ GitBasics/src/Main.java
@@ -34,6 +34,7 @@ public class Main {
 
         System.out.println("Today I gave interview with Here technogies managerial round");
         System.out.println("I am the most hardworking guy on the planet");
+        System.out.println("I woke up at 4 am today morning");
 
 
 
