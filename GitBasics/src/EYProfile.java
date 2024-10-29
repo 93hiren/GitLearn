b1c26fe1aae566065491d810110b63d62c7ab3a8
Hiren <galahiren3@gmail.com>
2024-10-29 12:58:53 +0530

    manifest

---
+++ GitBasics/src/EYProfile.java
@@ -0,0 +1,7 @@
+public class EYProfile {
+
+    public static void main(String[] args) {
+        System.out.println("I submited my profile to EY on 13 oct");
+        System.out.println("Ronak messaged me yesterday and I felt good");
+    }
+}
