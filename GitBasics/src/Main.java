79fd669b05e8b5da783138b21c33383464fce4dd
Hiren <galahiren3@gmail.com>
2024-10-30 19:59:51 +0530

    created mergemain

--- GitBasics/src/Main.java
+++ GitBasics/src/Main.java
@@ -20,48 +20,7 @@ public class Main {
         }
         System.out.println(sb.toString());
 
-        System.out.println("I am in Main Class");
-
-
-        System.out.println("Working Sunday and I love it after 1 month or so I will enjoy football" +
-                "and swiming ");
-
-        System.out.println("Merging from third main on Monday");
-        System.out.println("Gratefull for evrything that happend thank you so much for such divine blessing");
-
-        System.out.println("Sunday : Favorite City Mancity Football Club and one day I will watch love match in ethiad stadium");
-        System.out.println("Catch Live match Today between Arsnel and Liverpool");
-
-        System.out.println("Today I gave interview with Here technogies managerial round");
-        System.out.println("I am the most hardworking guy on the planet");
-        System.out.println("I woke up at 4 am today morning");
-
-        System.out.println("Tuesday still pending task");
-
-        System.out.println("I will work hard");
-
-
-        System.out.println("Thinking of buying a bike and a nice car");
-
-        System.out.println("I am a casanova");
-        System.out.println("I am rich brat Prodigal");
-
-
-
-        System.out.println("I got offer letter from EY today");
-
-        System.out.println("Today is dhanteras");
-
-        System.out.println("I will joining EY on Monday 4th November");
-        System.out.println("I will have 10000000rs by 10/29/2025 and " +
-                "I will make lot of money and attract lot of love and wealth in my life");
-        System.out.println("I hope I join Morgan Stanley from 15 Nov 2024");
-
-
-
-
-
-        System.out.println("I will buy bike RC/BMW");
+        System.out.println("I am in Main Class of merge main");
 
 
 
